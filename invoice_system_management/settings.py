79396dc192a79d208024f66073f042d41feb0816
omarcoming <steve.han11@gmail.com>
2024-05-23 16:14:52 -0700

    fixed .gitignore

--- invoice_system_management/settings.py
+++ invoice_system_management/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'invoice_system_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
@@ -91,9 +91,9 @@ DATABASES = {
         'USER': credentials.get('USER'),
         'PASSWORD': credentials.get('PASSWORD'),
         'HOST': 'localhost',
-        # 'PORT': '5432',
+        'PORT': '5432',
         ## marble yard postgres 14:
-        'PORT': '5434',
+        # 'PORT': '5434',
     }
 }
 
